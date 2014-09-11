82125ba3b1a0e4866ad32d02e8f4e9fbe457ad4a
ken61502 <kennyson@gmail.com>
2014-09-10 18:11:00 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KennySungInfo::Application.config.secret_key_base = 'fc8c9804d29bc938f19e93e463dd9bee96ff3405d69a9ba8c844d7bfa7f809ced88cc8df42ca03ae4cdfd3904357bebecc23c38213c80ee2365559b671dc3154'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KennySungInfo::Application.config.session_store :cookie_store, key: '_kenny-sung-info_session'
